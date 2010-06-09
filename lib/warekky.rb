eeebfdb8b64ebbcadba5866c751a708f3fef5d3d
akimatter <akm2000@gmail.com>
2010-06-10 06:47:28 +0900

    使用するEraグループにWarekky.era_group_classにクラスを指定するようにしました。

--- lib/warekky.rb
+++ lib/warekky.rb
@@ -2,8 +2,12 @@ require 'date'
 
 module Warekky
   autoload :Era, 'warekky/era'
+  autoload :EraGroup, 'warekky/era_group'
+  autoload :Ja, 'warekky/ja'
 
   class << self
+    attr_accessor :era_group_class
+
     # d: Date or Time
     def strftime(d, format)
     end
---
+++ lib/warekky/era_group.rb
@@ -0,0 +1,19 @@
+require 'warekky'
+
+module Warekky
+  class EraGroup
+    
+
+    class << self
+      def eras
+        @eras ||= []
+      end
+
+      def era(first, last, sym, sign, options = {})
+      end
+
+      
+    end
+
+  end
+end
