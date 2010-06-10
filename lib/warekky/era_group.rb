ad6c5defe90c6e03afa40b900ae89a996d3c8d58
akimatter <akm2000@gmail.com>
2010-06-10 15:06:48 +0900

    parseとstrftimeの実装をWarekky::Jaからその継承元のWarekky::EraGroupに移動しました。

--- lib/warekky/era_group.rb
+++ lib/warekky/era_group.rb
@@ -22,18 +22,45 @@ module Warekky
     def formats_regexp
       self.class.formats_regexp
     end
-
+    def replacements_before_parse
+      self.class.replacements_before_parse
+    end
     def era_extra_names
-      # インスタンスでは変更できないようにするので、引数を渡しません
       self.class.era_extra_names
     end
 
-    def strftime(d, format = {})
-      raise NotImplementedError, "#{self.class.name}#strftime is not implemented."
+    def parse(str, options = {})
+      str = str.dup
+      replacements_before_parse.each_with_index do |dic, idx|
+        str.gsub!(replacements_regexp_before_parse[idx]){|s| dic[s]}
+      end
+      era_dic = name_to_era
+      str.gsub!(era_replacements) do
+        md = Regexp.last_match
+        h = Hash[*md.captures]
+        h.delete(nil)
+        s = h.keys.first
+        era = era_dic[s]
+        raise ArgumentError, "Era not found for #{s.inspect}" unless era
+        era.to_ad_year(h[s]).to_s
+      end
+      Date.parse(str)
     end
 
-    def parse(str, options = {})
-      raise NotImplementedError, "#{self.class.name}#parse is not implemented."
+    def strftime(d, format)
+      return d.strftime(format) unless formats_regexp.match(format)
+      era = self[d]
+      era_year = era ? era.to_era_year(d.year) : d.year
+      format = format.dup
+      format.gsub!(formats_regexp) do
+        md = Regexp.last_match
+        s = md.captures.compact.first
+        rep = formats[s]
+        raise ArgumentError, "replacement not found for #{s.inspect}" unless rep
+        res = rep.call(era, era_year)
+        res
+      end
+      d.strftime(format)
     end
 
     def [](era_name_or_date_or_time)
@@ -46,6 +73,12 @@ module Warekky
       else
         raise ArgumentError, "#{self.class.name}#[] doesn't support #{era_name_or_date_or_time.inspect}"
       end
+    end
+
+    def replacements_regexp_before_parse
+      @replacements_regexp_before_parse ||=
+        replacements_before_parse.map{|replacements|
+        Regexp.union(*replacements.keys.map{|s| /(#{Regexp.escape(s)})/})}
     end
 
     class << self
@@ -67,11 +100,6 @@ module Warekky
         @name_to_era
       end
 
-      def era_extra_names(*args)
-        @era_extra_names = args unless args.empty?
-        @era_extra_names ||= []
-      end
-
       def era(first, last, key, sign, options = {})
         key = key.to_sym
         result = Era.new(key, sign, first, last, options).freeze
@@ -79,11 +107,21 @@ module Warekky
         result
       end
 
-      
+      def era_extra_names
+        eras.map{|era| era.options.keys}.flatten.uniq
+      end
 
       attr_accessor :parse_regexp_builder
       def parse_regexp(&block)
         @parse_regexp_builder = block
+      end
+
+      def replacements_before_parse
+        @replacements_before_parse ||= []
+      end
+
+      def replace_before_parse(*replacements)
+        @replacements_before_parse = replacements
       end
 
       def era_replacements
--- lib/warekky/ja.rb
+++ lib/warekky/ja.rb
@@ -1,26 +1,25 @@
 # -*- coding: utf-8 -*-
 require 'warekky'
 
-# http://ja.wikipedia.org/wiki/明治
-#		明治（めいじ）は、日本の元号の一つ。慶応の後、大正の前。明治元年1月1日（1868年1月25日）から
-#		明治45年（大正元年、1912年）7月30日までの期間を指す。明治天皇在位期間とほぼ一致する。
-#		ただし、実際に改元の詔書が出されたのは慶応4年9月8日（1868年10月23日）で、同年1月1日に遡って明治元年とすると定めた。
-#
-#		※明治5年までは旧暦を使用していたため、西暦（グレゴリオ暦）の年とは厳密には一致しない。詳細は明治元年〜5年の各年の項目を参照。
-#
-# http://ja.wikipedia.org/wiki/一世一元の詔
-#		一世一元の詔（いっせいいちげんのみことのり）は、慶応4年9月8日（グレゴリオ暦1868年10月23日）、
-#		慶応4年を改めて明治元年とするとともに、天皇一代に元号一つという一世一元の制を定めた詔。明治改元の詔ともいう。
-#
-# 1868/01/01, 1912/07/29 明治 - 45 # グレゴリオ暦では 1868/1/25 から明治元年。
-#	1912/07/30, 1926/12/24 大正 - 15
-#	1926/12/25, 1989/01/07 昭和 - 64
-#	1989/01/08,	---------- 平成
-
 module Warekky
   class Ja < EraGroup
-    era_extra_names :long, :short
+    # http://ja.wikipedia.org/wiki/明治
+    #		明治（めいじ）は、日本の元号の一つ。慶応の後、大正の前。明治元年1月1日（1868年1月25日）から
+    #		明治45年（大正元年、1912年）7月30日までの期間を指す。明治天皇在位期間とほぼ一致する。
+    #		ただし、実際に改元の詔書が出されたのは慶応4年9月8日（1868年10月23日）で、同年1月1日に遡って明治元年とすると定めた。
+    #
+    #		※明治5年までは旧暦を使用していたため、西暦（グレゴリオ暦）の年とは厳密には一致しない。詳細は明治元年〜5年の各年の項目を参照。
+    era('1868/01/01', '1912/07/29', :meiji , 'M', :long => '明治', :short => "明") # 1 - 45
+    era('1912/07/30', '1926/12/24', :taisho, 'T', :long => '大正', :short => "大") # 1 - 15
+    era('1926/12/25', '1989/01/07', :showa , 'S', :long => '昭和', :short => "昭") # 1 - 64
+    era('1989/01/08', nil					, :heisei, 'H', :long => '平成', :short => "平") # 1 -
 
+    # strftimeで使える記号
+		format('%G'){|era, era_year| era[:long] if era} # 明治/大正/昭和/平成
+    format('%g'){|era, era_year| era.sign if era}   # M/T/S/H
+		format('%n'){|era, era_year| '%02d' % era_year} # (元号での)年度
+
+    # parseで使われる元号毎の正規表現
     parse_regexp do |era|
       [ /(#{Regexp.escape(era.name)})(\d{1,2})/,
 				/(#{Regexp.escape(era.sign)})(\d{1,2})/,
@@ -28,68 +27,10 @@ module Warekky
 				/(#{Regexp.escape(era[:long])})(\d{1,2})/]
     end
 
-		format('%G'){|era, era_year| era[:long] if era}
-    format('%g'){|era, era_year| era.sign if era}
-		format('%n'){|era, era_year| '%02d' % era_year}
-
-    era('1868/01/01', '1912/07/29', :meiji , 'M', :long => '明治', :short => "明")
-    era('1912/07/30', '1926/12/24', :taisho, 'T', :long => '大正', :short => "大")
-    era('1926/12/25', '1989/01/07', :showa , 'S', :long => '昭和', :short => "昭")
-    era('1989/01/08', nil					, :heisei, 'H', :long => '平成', :short => "平")
-
-    DEFAULT_REPLACEMENTS_BEFORE_PARSE = [
-      {"元年" => "1年"}.freeze,
-      {"年" => ".", "月" => ".", "日" => ""}.freeze
-    ].freeze
-
-
-    attr_accessor :replacements_before_parse
-
-    def initialize
-      @replacements_before_parse = DEFAULT_REPLACEMENTS_BEFORE_PARSE.map{|hash| hash.dup}
-    end
-
-    def parse(str, options = {})
-      str = str.dup
-      replacements_before_parse.each_with_index do |dic, idx|
-        str.gsub!(replacements_regexp_before_parse[idx]){|s| dic[s]}
-      end
-      era_dic = name_to_era
-      str.gsub!(era_replacements) do
-        md = Regexp.last_match
-        h = Hash[*md.captures]
-        h.delete(nil)
-        s = h.keys.first
-        era = era_dic[s]
-        raise ArgumentError, "Era not found for #{s.inspect}" unless era
-        era.to_ad_year(h[s]).to_s
-      end
-      Date.parse(str)
-    end
-
-    def strftime(d, format)
-      return d.strftime(format) unless formats_regexp.match(format)
-      era = self[d]
-      era_year = era ? era.to_era_year(d.year) : d.year
-      format = format.dup
-      format.gsub!(formats_regexp) do
-        md = Regexp.last_match
-        s = md.captures.compact.first
-        rep = formats[s]
-        raise ArgumentError, "replacement not found for #{s.inspect}" unless rep
-        res = rep.call(era, era_year)
-        res
-      end
-      d.strftime(format)
-    end
-
-    private
-
-    def replacements_regexp_before_parse
-      @replacements_regexp_before_parse ||=
-        replacements_before_parse.map{|replacements|
-        Regexp.union(*replacements.keys.map{|s| /(#{Regexp.escape(s)})/})}
-    end
-
+    # parseを実行前に適用される置換
+    replace_before_parse(
+      {"元年" => "1年"},
+      {"年" => ".", "月" => ".", "日" => ""}
+      )
   end
 end
